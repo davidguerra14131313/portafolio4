e21d772bd40130637e495f8c77e7e19b2b39f8ca
david <davidguerra14131313@gmail.com>
2021-06-04 11:03:53 -0500

    acabando mi portafolio

--- js/animationmove.js
+++ js/animationmove.js
@@ -1,18 +1,34 @@
 window.addEventListener('scroll',function()
 {
-    let imagen=document.querySelector('.main__section-services')
-    console.log("hola mundo")
+    let imagen=document.querySelector('.main__section-services');
+    let content_service=document.querySelector('.services__container-service');
+    let conteiner_icon_arrow=document.querySelector('.ico-home__container');
+    let about_me=document.querySelector('.main__section-about-me__1');
     let altura=window.innerHeight;
-    
-    let distancia = imagen.getBoundingClientRect().top;
-    console.log(altura)
-    console.log(distancia)
+  
+    let distancia
+    distancia= imagen.getBoundingClientRect().top;
+    distancia= content_service.getBoundingClientRect().top;
+    let distancia2 
+    distancia2= about_me.getBoundingClientRect().top;
 
-    if(distancia <=altura)
+    if(distancia < altura & distancia2 < altura )
     {
+        
+        content_service.classList.add('aparece_content_service');
         imagen.classList.add('aparece');
+        conteiner_icon_arrow.classList.add('aparece_icon');
+
+        console.log(altura);
+        console.log(distancia);
     }
+   
     else{
         imagen.classList.remove('aparece');
+        content_service.classList.remove('aparece_content_service');
+        conteiner_icon_arrow.classList.remove('aparece_icon')
+
+     
     }
+   
 })
--- js/scroll.js
+++ js/scroll.js
@@ -3,3 +3,8 @@ const scrollTo =(section)=>
         let objsection=document.querySelector('#'+section);
         objsection.scrollIntoView({behavior:"smooth"})
 }
+const scrollTo_home =()=>
+{
+        let objsection = document.querySelector('#cabecera');
+        objsection.scrollIntoView({behavior:"smooth"})
+}
